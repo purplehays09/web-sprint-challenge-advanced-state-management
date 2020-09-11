29f1a586d6eed3d3de561ced3af22e80c76f1691
david hays <purplehays527@gmail.com>
2020-09-11 09:53:15 -0600

    I added the basic component structures

---
+++ smurfs/src/components/Smurf.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function Smurf(props){
+
+    return(
+        <div>
+            <h3>Smurf Level</h3>
+
+        </div>
+    )
+}
+
+export default Smurf
---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Smurf from './Smurf'
+
+
+function SmurfList(props){
+
+    return(
+        <div>
+            <h2>SmurfList Level</h2>
+
+            <Smurf
+            
+            />
+
+        </div>
+    )
+}
+
+export default SmurfList
