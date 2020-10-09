6f7f54225f06a52b0c2b8dc10832dedaa41d0d3b
david hays <purplehays527@gmail.com>
2020-10-09 09:59:14 -0600

    added the imports and the create store/Provider

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import {createStore} from 'redux'
+import {reducer} from "./reducers"
+import {Provider} from 'react-redux'
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById("root"));
